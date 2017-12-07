eee83ce1bf9b8b8949b8553c382946349428bbb8
rogergranada <roger.leitzke@gmail.com>
2017-12-07 18:01:20 -0200

    Added run.py

---
+++ sanntool/run.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import sys
+sys.path.insert(0, '..')
+import logging
+logger = logging.getLogger('run')
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+
+def main()
+
+if __name__ == "__main__":
+    main()
